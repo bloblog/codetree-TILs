acbba5d31f4dbfde87b491b2eaaeb86094f23e2d
HYESEUNG <109576718+bloblog@users.noreply.github.com>
2025-01-23 15:00:42 +0800

    Title: 벽 짚고 미로 탈출하기, Time: 3009ms, Memory: 11MB, Status: Time Limit Exceed - CodeTree

--- 250123/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
+++ 250123/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
@@ -24,15 +24,15 @@ public class Main {
 
         int d = 0;
         boolean flag = true; // 탈출 가능 여부
-        while (!checkRange(x, y, n)) {
+        while (checkRange(x, y, n)) {
             int nx = x + dr[d];
             int ny = y + dc[d];
             // 이동 불가능
-            if (!checkRange(nx, ny, n) && arr[nx][ny] == 1) {
+            if (checkRange(nx, ny, n) && arr[nx][ny] == 1) {
                 // 반시계 방향 전환
                 d = (d+1) % 4;
             } else {
-                if (checkRange(nx, ny, n)) {
+                if (!checkRange(nx, ny, n)) {
                     // 탈출(범위 벗어남)한 경우
                     // 위치 갱신 및 이동
                     x = nx;
@@ -96,36 +96,15 @@ public class Main {
     static int[] dr = new int[] {0, -1, 0, 1}; // 우 상 좌 하
     static int[] dc = new int[] {1, 0, -1, 0}; // 우 상 좌 하
     static boolean checkWall(int r, int c, int d, int[][] arr) {
-        int nr = r;
-        int nc = c;
-        if (d == 0) {
-            // 아래에 벽이 있어야 함
-            nr += dr[3];
-            nc += dc[3];
-            if (!checkRange(nr, nc, arr.length) && arr[nr][nc] == 1) return true;
-            else return false;
-        } else if (d == 1) {
-            // 오른쪽에 벽이 있어야 함
-            nr += dr[0];
-            nc += dc[0];
-            if (!checkRange(nr, nc, arr.length) && arr[nr][nc] == 1) return true;
-            else return false;
-        } else if (d == 2) {
-            // 위에 벽이 있어야 함
-            nr += dr[1];
-            nc += dc[1];
-            if (!checkRange(nr, nc, arr.length) && arr[nr][nc] == 1) return true;
-            else return false;
-        } else {
-            // 왼쪽에 벽이 있어야 함
-            nr += dr[2];
-            nc += dc[2];
-            if (!checkRange(nr, nc, arr.length) && arr[nr][nc] == 1) return true;
-            else return false;
-        }
+        // 각 방향에 따라 벽을 확인해야 할 상대 위치
+        int[] wallDr = {1, 0, -1, 0}; // 하 우 상 좌
+        int[] wallDc = {0, 1, 0, -1};
+        int nr = r + wallDr[d];
+        int nc = c + wallDc[d];
+        return checkRange(nr, nc, arr.length) && arr[nr][nc] == 1;
     }
 
     static boolean checkRange(int nx, int ny, int n) {
-        return nx < 0 || nx >= n || ny < 0 || ny >= n;
+        return nx >= 0 && nx < n && ny >= 0 && ny < n;
     }
 }
