08901052036aa938e674446fd5bf260595ffcaa8
HYESEUNG <109576718+bloblog@users.noreply.github.com>
2025-02-10 15:19:16 +0800

    Title: 벽이 있는 충돌 실험, Time: 1864ms, Memory: 157MB, Status: Passed - CodeTree

--- 250210/벽이 있는 충돌 실험/collision-experiment-with-wall.java
+++ 250210/벽이 있는 충돌 실험/collision-experiment-with-wall.java
@@ -14,12 +14,7 @@ public class Main {
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
             int[][] arr = new int[n][n];
-            Queue<Integer>[][] dir = new Queue[n][n];
-            for (int r = 0; r < n; r++) {
-                for (int c = 0; c < n; c++) {
-                    dir[r][c] = new LinkedList<>();
-                }
-            }
+            Queue<int[]> queue = new LinkedList<>();
 
             String[] dirMap = new String[] {"U", "D", "R", "L"};
             for (int j = 0; j < m; j++) {
@@ -28,66 +23,65 @@ public class Main {
                 int y = Integer.parseInt(st.nextToken())-1;
                 String d = st.nextToken();
                 arr[x][y]++;
-                dir[x][y].add(Arrays.asList(dirMap).indexOf(d));
+                queue.add(new int[] {x, y, Arrays.asList(dirMap).indexOf(d)});
             }
 
-            for (int time = 0; time < n + n/2; time++) {
+            for (int time = 0; time < n*2; time++) {
                 int[][] copyArr = new int[n][n];
                 for (int k = 0; k < n; k++) {
                     copyArr[k] = Arrays.copyOf(arr[k], n);
                 }
 
-                // 한 칸 이동
-                for (int r = 0; r < n; r++) {
-                    for (int c = 0; c < n; c++) {
-                        if (arr[r][c] == 1) {
-                            int d = dir[r][c].poll();
-                            int nr = r + dr[d];
-                            int nc = c + dc[d];
-                            // 벽에 부딪히면 방향 전환
-                            if (nr < 0 || nr >= n || nc < 0 || nc >= n) {
-                                changeDir(r, c, d, dir);
-                                continue;
-                            }
-                            copyArr[nr][nc]++;
-                            copyArr[r][c]--;
-                            dir[nr][nc].add(d);
-                        }
+                // 한 칸씩 이동
+                int iter = queue.size();
+                while (iter-- > 0) {
+                    int[] q = queue.poll();
+                    int r = q[0];
+                    int c = q[1];
+                    int d = q[2];
+
+                    int nr = r + dr[d];
+                    int nc = c + dc[d];
+                    // 벽에 부딪히면 방향 전환
+                    if (nr < 0 || nr >= n || nc < 0 || nc >= n) {
+                        changeDir(r, c, d, queue);
+                        continue;
                     }
+                    copyArr[nr][nc]++;
+                    copyArr[r][c]--;
+                    queue.add(new int[] {nr, nc, d});
                 }
 
                 // 구슬 사라짐
-                for (int r = 0; r < n; r++) {
-                    for (int c = 0; c < n; c++) {
-                        if (copyArr[r][c] > 1) {
-                            copyArr[r][c] = 0;
-                            dir[r][c].clear();
-                        }
+                int size = queue.size();
+                for (int j = 0; j < size; j++) {
+                    int[] a = queue.poll();
+                    if (copyArr[a[0]][a[1]] == 1) {
+                        queue.add(new int[] {a[0], a[1], a[2]});
                     }
                 }
+
+                for (int x = 0; x < n; x++) {
+                    for (int y = 0; y < n; y++) {
+                        if (copyArr[x][y] > 1) copyArr[x][y] = 0;
+                    }
+                }
+
 
                 // 원래 배열에 복사
                 for (int k = 0; k < n; k++) {
                     arr[k] = Arrays.copyOf(copyArr[k], n);
                 }
             }
-
-            // 남은 구슬 카운트
-            int cnt = 0;
-            for (int r = 0; r < n; r++) {
-                for (int c = 0; c < n; c++) {
-                    if (arr[r][c] == 1) cnt++;
-                }
-            }
-            System.out.println(cnt);
+            System.out.println(queue.size());
         }
     }
 
-    static void changeDir(int r, int c, int d, Queue<Integer>[][] dir) {
-        if (d == 0) dir[r][c].add(1);
-        else if (d == 1) dir[r][c].add(0);
-        else if (d == 2) dir[r][c].add(3);
-        else if (d == 3) dir[r][c].add(2);
+    static void changeDir(int r, int c, int d, Queue<int[]> queue) {
+        if (d == 0) queue.add(new int[] {r, c, 1});
+        else if (d == 1) queue.add(new int[] {r, c, 0});
+        else if (d == 2) queue.add(new int[] {r, c, 3});
+        else if (d == 3) queue.add(new int[] {r, c, 2});
     }
 
     static int[] dr = new int[] {-1, 1, 0, 0}; // U D R L
