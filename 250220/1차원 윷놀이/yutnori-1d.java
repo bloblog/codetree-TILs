23e45b8149d6e89ac0a186cc53b8f0b2778972b5
HYESEUNG <109576718+bloblog@users.noreply.github.com>
2025-02-20 10:47:10 +0800

    Title: 1차원 윷놀이, Time: 177ms, Memory: 19MB, Status: Passed - CodeTree

--- 250220/1차원 윷놀이/yutnori-1d.java
+++ 250220/1차원 윷놀이/yutnori-1d.java
@@ -43,7 +43,7 @@ public class Main {
             return;
         }
 
-        for (int i = 1; i <= k; i++) {
+        for (int i = 0; i < k; i++) {
             sel[idx] = i;
             getPerm(idx+1, sel);
         }
