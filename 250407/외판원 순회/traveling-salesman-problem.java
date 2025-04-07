c605ba2adcb44ef457157baa1b7147d0be718870
HYESEUNG <109576718+bloblog@users.noreply.github.com>
2025-04-07 12:11:45 +0900

    Title: 외판원 순회, Time: 120ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250407/외판원 순회/traveling-salesman-problem.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        cost = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cost[i][j] = sc.nextInt();
+            }
+        }
+
+        min = 987654321;
+        perm(0, new int[n], new boolean[n]);
+        System.out.println(min);
+    }
+
+    public static int n, min;
+    public static int[][] cost;
+
+    public static void calc(int[] sel) {
+        int sum = 0;
+        int row = 0;
+        for (int i = 0; i < n; i++) {
+            sum += cost[row][sel[i]];
+            row = sel[i];
+        }
+        min = Math.min(min, sum);
+    }
+
+    public static void perm(int idx, int[] sel, boolean[] visited) {
+        
+        if (idx == n-1) {
+            calc(sel);
+            return;
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (!visited[i]) {
+                sel[idx] = i;
+                visited[i] = true;
+                perm(idx+1, sel, visited);
+                visited[i] = false;
+            }
+        }
+    }
+}
